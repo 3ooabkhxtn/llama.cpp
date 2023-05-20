5fc0e1b479211a75eb3970e45a3aa1ce0e3e5d17
Daniel <daniel@fake.local>
2023-05-20 07:20:34 +0000

    - removed sse from qx_1 as not tested

--- ggml.c
+++ ggml.c
@@ -1400,87 +1400,6 @@ static void quantize_row_q8_1(const float * restrict x, void * restrict vy, int 
         _mm_storeu_si128((__m128i *)(y[i].qs + 16), ni4);
 #endif
     }
-#elif defined(__SSSE__)
-    for (int i = 0; i < nb; i++) {
-        // Load elements into 8 SSE vectors
-        __m128 v0 = _mm_loadu_ps( x );
-        __m128 v1 = _mm_loadu_ps( x + 4 );
-        __m128 v2 = _mm_loadu_ps( x + 8 );
-        __m128 v3 = _mm_loadu_ps( x + 12 );
-        __m128 v4 = _mm_loadu_ps( x + 16 );
-        __m128 v5 = _mm_loadu_ps( x + 20 );
-        __m128 v6 = _mm_loadu_ps( x + 24 );
-        __m128 v7 = _mm_loadu_ps( x + 28 );
-        x += 32;
-
-        // Compute max(abs(e)) for the block
-        const __m128 signBit = _mm_set1_ps( -0.0f );
-        __m128 maxAbs = _mm_andnot_ps( signBit, v0 );
-        maxAbs = _mm_max_ps( maxAbs, _mm_andnot_ps( signBit, v1 ) );
-        maxAbs = _mm_max_ps( maxAbs, _mm_andnot_ps( signBit, v2 ) );
-        maxAbs = _mm_max_ps( maxAbs, _mm_andnot_ps( signBit, v3 ) );
-        maxAbs = _mm_max_ps( maxAbs, _mm_andnot_ps( signBit, v4 ) );
-        maxAbs = _mm_max_ps( maxAbs, _mm_andnot_ps( signBit, v5 ) );
-        maxAbs = _mm_max_ps( maxAbs, _mm_andnot_ps( signBit, v6 ) );
-        maxAbs = _mm_max_ps( maxAbs, _mm_andnot_ps( signBit, v7 ) );
-
-        __m128 max4 = _mm_max_ps( maxAbs, _mm_movehl_ps( maxAbs, maxAbs ) );
-        max4 = _mm_max_ss( max4, _mm_movehdup_ps( max4 ) );
-        const float maxScalar = _mm_cvtss_f32( max4 );
-
-        // Quantize these floats
-        const float d = maxScalar / 127.f;
-        y[i].d = GGML_FP32_TO_FP16(d);
-        const float id = ( maxScalar != 0.0f ) ? 127.f / maxScalar : 0.0f;
-        const __m128 mul = _mm_set1_ps( id );
-
-        // Apply the multiplier
-        v0 = _mm_mul_ps( v0, mul );
-        v1 = _mm_mul_ps( v1, mul );
-        v2 = _mm_mul_ps( v2, mul );
-        v3 = _mm_mul_ps( v3, mul );
-        v4 = _mm_mul_ps( v4, mul );
-        v5 = _mm_mul_ps( v5, mul );
-        v6 = _mm_mul_ps( v6, mul );
-        v7 = _mm_mul_ps( v7, mul );
-
-        // Round to nearest integer
-        v0 = _mm_round_ps( v0, _MM_ROUND_NEAREST );
-        v1 = _mm_round_ps( v1, _MM_ROUND_NEAREST );
-        v2 = _mm_round_ps( v2, _MM_ROUND_NEAREST );
-        v3 = _mm_round_ps( v3, _MM_ROUND_NEAREST );
-        v4 = _mm_round_ps( v4, _MM_ROUND_NEAREST );
-        v5 = _mm_round_ps( v5, _MM_ROUND_NEAREST );
-        v6 = _mm_round_ps( v6, _MM_ROUND_NEAREST );
-        v7 = _mm_round_ps( v7, _MM_ROUND_NEAREST );
-
-        // Convert floats to integers
-        __m128i ni0 = _mm_cvtps_epi32( v0 );
-        __m128i ni1 = _mm_cvtps_epi32( v1 );
-        __m128i ni2 = _mm_cvtps_epi32( v2 );
-        __m128i ni3 = _mm_cvtps_epi32( v3 );
-        __m128i ni4 = _mm_cvtps_epi32( v4 );
-        __m128i ni5 = _mm_cvtps_epi32( v5 );
-        __m128i ni6 = _mm_cvtps_epi32( v6 );
-        __m128i ni7 = _mm_cvtps_epi32( v7 );
-
-        // Compute the sum of the quants and set y[i].s
-        const __m128i s0 = _mm_add_epi32(_mm_add_epi32(ni0, ni1), _mm_add_epi32(ni2, ni3));
-        const __m128i s1 = _mm_add_epi32(_mm_add_epi32(ni4, ni5), _mm_add_epi32(ni6, ni7));
-        y[i].s = d * hsum_i32_4(_mm_add_epi32(s0, s1));
-
-        // Convert int32 to int16
-        ni0 = _mm_packs_epi32( ni0, ni1 );
-        ni2 = _mm_packs_epi32( ni2, ni3 );
-        ni4 = _mm_packs_epi32( ni4, ni5 );
-        ni6 = _mm_packs_epi32( ni6, ni7 );
-        // Convert int16 to int8
-        ni0 = _mm_packs_epi16( ni0, ni2 );
-        ni4 = _mm_packs_epi16( ni4, ni6 );
-
-        _mm_storeu_si128((__m128i *)(y[i].qs +  0), ni0);
-        _mm_storeu_si128((__m128i *)(y[i].qs + 16), ni4);
-    }
 #else
     // scalar
     quantize_row_q8_1_reference(x, y, k);
@@ -1573,60 +1492,6 @@ static void dequantize_row_q4_1(const block_q4_1 * restrict x, float * restrict 
 
     const int nb = k / qk;
 
-#if defined(__SSSE3__)
-    assert(qk == 32);
-
-    const __m128i lowMask  = _mm_set1_epi8(0x0F);
-
-    __m128 fx[8];
-
-    assert(sizeof(fx) == 8 * sizeof(__m128));
-
-    int j = 0;
-    for (int i = 0; i < nb; i++) {
-        const __m128 d = _mm_set1_ps(GGML_FP16_TO_FP32(x[i].d));
-        const __m128 m = _mm_set1_ps(GGML_FP16_TO_FP32(x[i].m));
-
-        const __m128i x_ = _mm_loadu_si128((const __m128i *)(x[i].qs));
-
-        __m128i x0 = _mm_and_si128(lowMask, x_);
-        __m128i x1 = _mm_and_si128(lowMask, _mm_srli_epi64(x_, 4));
-
-        const __m128i x0_0 = _mm_shuffle_epi32(x0, _MM_SHUFFLE(0, 0, 0, 0));
-        const __m128i x0_1 = _mm_shuffle_epi32(x0, _MM_SHUFFLE(1, 1, 1, 1));
-        const __m128i x0_2 = _mm_shuffle_epi32(x0, _MM_SHUFFLE(2, 2, 2, 2));
-        const __m128i x0_3 = _mm_shuffle_epi32(x0, _MM_SHUFFLE(3, 3, 3, 3));
-
-        const __m128i x1_0 = _mm_shuffle_epi32(x1, _MM_SHUFFLE(0, 0, 0, 0));
-        const __m128i x1_1 = _mm_shuffle_epi32(x1, _MM_SHUFFLE(1, 1, 1, 1));
-        const __m128i x1_2 = _mm_shuffle_epi32(x1, _MM_SHUFFLE(2, 2, 2, 2));
-        const __m128i x1_3 = _mm_shuffle_epi32(x1, _MM_SHUFFLE(3, 3, 3, 3));
-
-        const __m128 fx0_0 = _mm_cvtpi8_ps(_mm_movepi64_pi64(x0_0));
-        const __m128 fx0_1 = _mm_cvtpi8_ps(_mm_movepi64_pi64(x0_1));
-        const __m128 fx0_2 = _mm_cvtpi8_ps(_mm_movepi64_pi64(x0_2));
-        const __m128 fx0_3 = _mm_cvtpi8_ps(_mm_movepi64_pi64(x0_3));
-
-        const __m128 fx1_0 = _mm_cvtpi8_ps(_mm_movepi64_pi64(x1_0));
-        const __m128 fx1_1 = _mm_cvtpi8_ps(_mm_movepi64_pi64(x1_1));
-        const __m128 fx1_2 = _mm_cvtpi8_ps(_mm_movepi64_pi64(x1_2));
-        const __m128 fx1_3 = _mm_cvtpi8_ps(_mm_movepi64_pi64(x1_3));
-
-        _mm_prefetch(&x[i] + sizeof(block_q4_0), _MM_HINT_T0);
-
-        fx[0] = _mm_add_ps(_mm_mul_ps(fx0_0, d), m);
-        fx[1] = _mm_add_ps(_mm_mul_ps(fx0_1, d), m);
-        fx[2] = _mm_add_ps(_mm_mul_ps(fx0_2, d), m);
-        fx[3] = _mm_add_ps(_mm_mul_ps(fx0_3, d), m);
-        fx[4] = _mm_add_ps(_mm_mul_ps(fx1_0, d), m);
-        fx[5] = _mm_add_ps(_mm_mul_ps(fx1_1, d), m);
-        fx[6] = _mm_add_ps(_mm_mul_ps(fx1_2, d), m);
-        fx[7] = _mm_add_ps(_mm_mul_ps(fx1_3, d), m);
-
-        memcpy(y + j, fx, 8 * sizeof(__m128));
-        j += 32;
-    }
-#else
     for (int i = 0; i < nb; i++) {
         const float d = GGML_FP16_TO_FP32(x[i].d);
         const float m = GGML_FP16_TO_FP32(x[i].m);
@@ -1639,7 +1504,6 @@ static void dequantize_row_q4_1(const block_q4_1 * restrict x, float * restrict 
             y[i*qk + j + qk/2] = x1*d + m;
         }
     }
-#endif
 }
 
 static void dequantize_row_q5_0(const block_q5_0 * restrict x, float * restrict y, int k) {
